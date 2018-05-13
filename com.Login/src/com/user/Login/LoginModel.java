f528b8a1998e71d30be287f0fac4eee588cf2bc2
mieraf.kiros <mierafkiros.luel@mail.polimi.it>
2018-05-13 08:09:05 +0200

    Committed webproject

---
+++ com.Login/src/com/user/Login/LoginModel.java
@@ -0,0 +1,10 @@
+package com.user.Login;
+
+public class LoginModel {
+
+	public String getType(String login, String paswd) {
+		System.out.println(login+"..,"+paswd);
+		return null;
+	}
+
+}
---
+++ com.Login/src/com/user/Login/loginController.java
@@ -0,0 +1,22 @@
+package com.user.Login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class loginController implements Controller {
+	
+	LoginModel lmodel;
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String login=req.getParameter("login");
+		String paswd= req.getParameter("pswd");
+		
+		String type=lmodel.getType(login, paswd);
+		return null;
+	}
+
+}
